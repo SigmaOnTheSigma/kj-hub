8f7bfef811818a64fb174db68983564d94b659a7
SigmaOnTheSigma <carterwitte434@gmail.com>
2025-02-02 13:35:10 -0500

    Update Criminal.lua

--- Criminal.lua
+++ Criminal.lua
@@ -3,7 +3,7 @@ local player = game.Players.LocalPlayer
 local char = player.Character
 
 local crimpart = game.Workspace["Criminals Spawn"].SpawnLocation
-local pos = "-920.510803, 92.2271957, 2138.27002, 0, 0, -1, 0, 1, 0, 1, 0, 0"
+local pos = -920.510803, 92.2271957, 2138.27002, 0, 0, -1, 0, 1, 0, 1, 0, 0
 local hrp = char.HumanoidRootPart
 
 

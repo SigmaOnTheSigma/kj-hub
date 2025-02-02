9afcac8da00b3f2b699afbd534216aeec97f2962
SigmaOnTheSigma <carterwitte434@gmail.com>
2025-02-02 13:54:25 -0500

    Update TeleportToWin.lua

--- TeleportToWin.lua
+++ TeleportToWin.lua
@@ -1,6 +1,6 @@
 local player = game.Players.LocalPlayer
 local char = player.Character
 local hrp = char.HumanoidRootPart
-local teleportto = game.Workspace["Purchase Station"].PurchasePlate
+local teleportto = game.Workspace["Shop sign"].2
 
 hrp.CFrame = teleportto.CFrame

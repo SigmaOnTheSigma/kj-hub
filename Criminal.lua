a30af10b5f42d84e8699105cb1f9505d049d7cd8
SigmaOnTheSigma <carterwitte434@gmail.com>
2025-02-02 13:39:37 -0500

    Update Criminal.lua

--- Criminal.lua
+++ Criminal.lua
@@ -3,7 +3,6 @@ local player = game.Players.LocalPlayer
 local char = player.Character
 
 local crimpart = game.Workspace["Criminals Spawn"].SpawnLocation
-local pos = -920.510803, 92.2271957, 2138.27002, 0, 0, -1, 0, 1, 0, 1, 0, 0
 local hrp = char.HumanoidRootPart
 
 
@@ -11,4 +10,4 @@ crimpart.CanCollide = false
 crimpart.CFrame = hrp.CFrame
 wait(0.1)
 
-crimpart.CFrame = pos
+crimpart.CFrame = -920.510803, 92.2271957, 2138.27002, 0, 0, -1, 0, 1, 0, 1, 0, 0

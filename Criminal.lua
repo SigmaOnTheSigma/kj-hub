e47bae1477dab1974dc9c61802fa70d32934e2de
SigmaOnTheSigma <carterwitte434@gmail.com>
2025-02-02 13:40:57 -0500

    Update Criminal.lua

--- Criminal.lua
+++ Criminal.lua
@@ -10,4 +10,4 @@ crimpart.CanCollide = false
 crimpart.CFrame = hrp.CFrame
 wait(0.1)
 
-crimpart.CFrame = -920.510803, 92.2271957, 2138.27002, 0, 0, -1, 0, 1, 0, 1, 0, 0
+crimpart.CFrame = "-920.510803, 92.2271957, 2138.27002, 0, 0, -1, 0, 1, 0, 1, 0, 0"

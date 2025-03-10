302d2e43813319f290471e9af10f3680deff93ed
SigmaOnTheSigma <carterwitte434@gmail.com>
2025-03-10 17:21:37 -0400

    Update Shinjimovs.lua

--- Shinjimovs.lua
+++ Shinjimovs.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet('https://github.com/SigmaOnTheSigma/kj-hub/blob/8e5ed7862f1196122ec33b920c9f27e532b4d2a4/Moveset'))()
+loadstring(game:HttpGet('https://github.com/SigmaOnTheSigma/kj-hub/blob/Movesetid/Moveset'))()
 
 local player = game.Players.LocalPlayer
 local playerGui = player.PlayerGui

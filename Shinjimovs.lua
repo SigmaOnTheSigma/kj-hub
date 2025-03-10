2058db20f85a553863eef18430df0e0047cb5ee7
SigmaOnTheSigma <carterwitte434@gmail.com>
2025-03-10 16:47:49 -0400

    Create Shinjimovs.lua

---
+++ Shinjimovs.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://pastebin.com/mYxg8G1e'))()
+loadstring(game:HttpGet('https://github.com/SigmaOnTheSigma/kj-hub/blob/8e5ed7862f1196122ec33b920c9f27e532b4d2a4/Moveset'))()
